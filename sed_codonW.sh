8c461afa6d5050899bc3c5173651569abdbae5d5
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-06-14 16:03:03 +0800

    Update sed_codonW.sh

--- sed_codonW.sh
+++ sed_codonW.sh
@@ -4,7 +4,10 @@ sed 's/           	/\t/g' CBI_CAI_bycodonW.txt > temp
 mv temp CBI_CAI_bycodonW.txt
 sed 's/\t\t/\t/g' CBI_CAI_bycodonW.txt > temp
 mv temp CBI_CAI_bycodonW.txt
-sed -i 's/         //g' CBI_CAI_bycodonW.txt
 sed -i 's/      //g' CBI_CAI_bycodonW.txt
 sed -i 's/\t\t/\t/g' CBI_CAI_bycodonW.txt
-
+sed -i 's/   //g' CBI_CAI_bycodonW.txt
+sed -i 's/\t\t/\t/g' CBI_CAI_bycodonW.txt
+sed -i 's/  //g' CBI_CAI_bycodonW.txt
+sed -i 's/ //g' CBI_CAI_bycodonW.txt
+sed -i 's/\t\t/\t/g' CBI_CAI_bycodonW.txt

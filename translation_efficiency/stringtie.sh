4032caa0d707b0a66235d3f573041703e7de456e
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-07-02 13:37:47 +0800

    Add files via upload

---
+++ translation_efficiency/stringtie.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+for item in $(ls *.sam)
+do
+	echo "sam_${item%.*}"		
+	
+	samtools sort -@ 8 -o ${item%.*}.bam ${item%.*}.sam
+	
+	echo "str_${item%.*}"
+	stringtie -p 30 --rf -e -G /media/hp/disk1/DYY/reference/annotation/hg19/ref.gtf -o ./${item%.*}.gtf -l ${item%.*} ${item%.*}.bam -A ${item%.*}_abund.out
+
+done
+
+

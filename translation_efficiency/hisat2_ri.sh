360b5a514917adf9e0c7440050c3c1ac337059ff
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-07-03 20:31:58 +0800

    Update hisat2_ri.sh

--- translation_efficiency/hisat2_ri.sh
+++ translation_efficiency/hisat2_ri.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for item in $(ls *.fq)
+for item in $(ls *.sra)
 do
 	echo "hi_${item%.*}"
-	hisat2 -p 30 -t --dta --rna-strandness F -x /media/hp/disk1/DYY/reference/index/hg19/genome_index -U ./${item%.*}.fq -S ./${item%.*}.sam 2>>./ribo_mapping_repo.txt
+	hisat2 -p 30 -t --dta --rna-strandness F -x /media/hp/disk1/DYY/reference/index/C_elegans_Ensl_WBcel235/genome_index -U ./fastq/${item%.*}.sra.fastq -S ./${item%.*}.sam 2>> ./ribo_mapping_repo.txt
 	
 done

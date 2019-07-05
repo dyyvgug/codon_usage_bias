febd49d5c88301d1df78f6f4780a523c4d1de07e
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-07-05 10:09:49 +0800

    Add files via upload

---
+++ NC_GC3_TPM.R
@@ -0,0 +1,63 @@
+library('ggplot2')
+species = "Caenorhabditis_elegans"
+species_abb = "Ce"
+setwd(paste0("/media/hp/disk1/DYY/reference/annotation/",species,"/ref"))
+codonW = read.table(file = 'CBI_CAI_bycodonW.txt',sep = '\t',header = T,quote = "")
+codonW = codonW[-16]
+codonW[codonW == '*****'] <-NA
+codonW = codonW[complete.cases(codonW),]
+codonW$Nc = as.numeric(as.character(codonW$Nc))
+#=================================================================================
+# Nc-plot
+#=================================================================================
+# Custom standard curve function
+st.fun <- function(gc3){
+  2 + gc3 + 29/(gc3**2 + (1-gc3)**2)
+}
+# Scatter plot + standard curve
+svg(file = "Nc_plot.svg")
+#jpeg(file = "Nc_plot.jpg",width=1000,height=600)
+ggplot(codonW, aes(x = GC3s, y = Nc)) + 
+  geom_point(shape = 16, size = 0.1) + 
+  stat_function(fun = st.fun, size = 1,color = 'blue') +
+  scale_y_continuous(breaks = c(25,30,35,40,45,50,55,60,65)) +
+  theme(axis.line = element_line(colour = 'black')) +
+  labs(title = paste0(species_abb,"_Nc_GC3s"))+
+  xlab('GC3s')
+dev.off()
+#===================================================================================
+# Nc and mRNA levels
+#===================================================================================
+mRNA_level = read.table("/media/hp/Katniss/DYY/aligned/C_elegans_Ensl_WBcel235/experiment2/SRR1056310.gtf.T",sep = '\t',header = F)
+names(mRNA_level) = c("rna_id","gene_name","FPKM","TPM")
+Nc_TPM = merge(codonW,mRNA_level,by.x = "transcription_id",by.y = "rna_id",all = T)
+Nc_TPM[Nc_TPM == 0] <- NA
+Nc_TPM = Nc_TPM[complete.cases(Nc_TPM),] # Or df = na.omit(df)
+if(FALSE) # Multi-line comment
+{
+  mid<-mean(Nc_TPM$TPM)
+  Nc_TPM <- ggplot(Nc_TPM,aes(x = GC3s,y = Nc,color = TPM))+
+    geom_point(shape = 16,size = 0.1)+
+    stat_function(fun = st.fun,size = 1,color = "black")+
+    scale_y_continuous(breaks = c(25,30,35,40,45,50,55,60,65))+
+    scale_color_gradient(low="green", high="red")+
+    theme(axis.line = element_line(colour = 'black')) +
+    labs(title = paste0(species_abb,"_Nc_GC3s"))+
+    xlab('GC3s')
+  Nc_TPM
+}
+sum(Nc_TPM$TPM > 500)
+Nc_TPM[] <- lapply(Nc_TPM, function(x) ifelse(x > 500, 500, x))
+svg(file = "Nc_TPM_plot.svg")
+Nc_TPM <- ggplot(Nc_TPM,aes(x = GC3s,y = Nc,color = TPM))+
+  geom_point(shape = 16,size = 0.1)+
+  stat_function(fun = st.fun,size = 1,color = "black")+
+  scale_y_continuous(breaks = c(25,30,35,40,45,50,55,60,65))+
+  scale_color_gradientn(colours = rainbow(5))+
+  theme(axis.line = element_line(colour = 'black')) +
+  labs(title = paste0(species_abb,"_Nc_GC3s"))+
+  xlab('GC3s')+
+  theme_bw() +
+  theme(axis.line = element_line(colour = "black"))
+Nc_TPM
+dev.off()

8837827ae9b415fd09f1153c80d2b09f75a3a35d
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-08-06 14:21:34 +0800

    Update hisat2.sh

--- translation_efficiency/hisat2.sh
+++ translation_efficiency/hisat2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir aligned
+mkdir ../aligned
 for item in $(ls *.sra)
 do
 	echo "hi_${item%.*}"
-	hisat2 -p 30 -t --dta -x /media/hp/disk1/DYY/reference/index/hg19/genome_index -1 ./${item%.*}.sra_1.fastq -2 ./${item%.*}.sra_2.fastq -S ./aligned/${item%.*}.sam 2>> ./aligned/mapping_repo.txt
+	hisat2 -p 30 -t --dta -x /media/hp/disk1/DYY/reference/index/hg19/genome_index -1 ./${item%.*}.sra_1.fastq -2 ./${item%.*}.sra_2.fastq -S ../aligned/${item%.*}.sam 2>> ../aligned/mapping_repo.txt
 done

a5706006683a5eb7dd43e03e2590160ac9eb496b
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-09-06 15:21:28 +0800

    Update hisat2_ri.sh

--- translation_efficiency/hisat2_ri.sh
+++ translation_efficiency/hisat2_ri.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir ../aligned_ri
-for item in $(ls *.sra)
+for item in $(ls *.fastq)
 do
         echo "hi_${item%.*}"
-        hisat2 -p 30 -t --dta --rna-strandness F -x /media/hp/disk1/DYY/reference/index/Solanum_lycopersicum/genome_index -U ./${item%.*}.sra.fastq.gz -S ../aligned_ri/${item%.*}.sam 2>> ../aligned_ri/ribo_mapping_repo.txt
+        hisat2 -p 30 -t --dta --rna-strandness F -x /media/hp/disk1/DYY/reference/index/Solanum_lycopersicum/genome_index -U ./${item%.*}.sra.fastq -S ../aligned_ri/${item%.*}.sam 2>> ../aligned_ri/ribo_mapping_repo.txt
         
 done

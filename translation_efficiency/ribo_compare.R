dda872a8479f1f691197ed9c7aad7eb4f81eae41
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-09-18 16:46:52 +0800

    Update ribo_compare.R

--- translation_efficiency/ribo_compare.R
+++ translation_efficiency/ribo_compare.R
@@ -8,8 +8,10 @@ names(other_ribo_gene) = c("ribo_name","RNA_TPM","ribo_TPM","tag")
 names(hE_hT_ribo_gene) = c("ribo_name","RNA_TPM","ribo_TPM","tag")
 ribo_gene = merge(other_ribo_gene,hE_hT_ribo_gene,all = T)
 png("40_compare_ribo_rnaTPM.png")
-p <- boxplot(other_ribo_gene$RNA_TPM,hE_hT_ribo_gene$RNA_TPM,names=c('OTHER','hiEXPhiTRA'),col=c("green","red"))
+p <- boxplot(log(other_ribo_gene$RNA_TPM),log(hE_hT_ribo_gene$RNA_TPM),
+             names=c('OTHER','hiEXPhiTRA'),col=c("green","red"),ylab = 'log(TPM)')
 dev.off()
 png("40_compare_ribo_riboTPM.png")
-p <- boxplot(other_ribo_gene$ribo_TPM,hE_hT_ribo_gene$ribo_TPM,names=c('OTHER','hiEXPhiTRA'),col=c("green","red"))
+p <- boxplot(log(other_ribo_gene$ribo_TPM),log(hE_hT_ribo_gene$ribo_TPM),
+             names=c('OTHER','hiEXPhiTRA'),col=c("green","red"),ylab = 'log(TPM)')
 dev.off()

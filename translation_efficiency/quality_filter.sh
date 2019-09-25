d232a7920d9ddca5773ac43e4c82cc97629604c4
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-09-25 16:08:45 +0800

    Delete quality_filter.sh

--- translation_efficiency/quality_filter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir ../quality
-for item in $(ls *.sra)
-do
-	echo "quality_${item%.*}"
-	fastq_quality_filter -q 20 -p 80 -Q 33 -i ../clipper_fastq/${item%.*}_tri.fastq -o ../quality/${item%.*}_qua.fastq
-done
-

33d7bf1f078ad9910b38c1ad99e28fee1e22a77a
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-09-25 16:07:58 +0800

    Update clipper_ribo_seqence.sh

--- translation_efficiency/clipper_ribo_seqence.sh
+++ translation_efficiency/clipper_ribo_seqence.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 cd ./fq
 mkdir ../clipper_fastq
+mkdir ../quality
 for item in $(ls *.sra)
 do
 	echo "clipper_${item%.*}"
 	fastx_clipper -a CTGTAGGCA -l 20 -d 0 -Q 33 -i ./${item%.*}.sra.fastq -o ../clipper_fastq/${item%.*}_tri.fastq
-	
 
-
+	echo "quality_${item%.*}"
+	fastq_quality_filter -q 20 -p 80 -Q 33 -i ../clipper_fastq/${item%.*}_tri.fastq -o ../quality/${item%.*}_qua.fastq
 done
+
+cd ../quality
+rename 's/_qua//' *
 

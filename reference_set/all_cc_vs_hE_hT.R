5cd8ae224226fd771135bd5f77d80f95b37d4e2d
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-10-11 15:55:21 +0800

    Update all_cc_vs_hE_hT.R

--- reference_set/all_cc_vs_hE_hT.R
+++ reference_set/all_cc_vs_hE_hT.R
@@ -4,10 +4,14 @@
 # The TPM value of the intersection gene is compared with the TPM value of the gene with high 
 # expression & high translation.
 #================================================================================================
+sra = 'SRR4175354'
+species = 'Saccharomyces_cerevisiae'
+experiment = 'experiment2'
+sra_path = paste0('/home/hp/Desktop/other_riboseq/',species,'/',experiment,'/aligned/ribo_num/')
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
 s = read.table("CC_only_symbol.txt",header = F,quote = '',sep = '\t')
-TPM =  read.table('SRR6930636_riboNum.txt',header = T,quote = '',sep = '\t')
-hE_hT = read.table('SRR6930636_hE_ht_gene.txt',header = T,quote = '',sep = '\t')
+TPM =  read.table(paste0(sra_path,sra,'_riboNum.txt'),header = T,quote = '',sep = '\t')
+hE_hT = read.table(paste0(sra_path,sra,'_hiE_ht_gene.txt'),header = T,quote = '',sep = '\t')
 names(s) = "Gene.Name"
 s_TPM = merge(s,TPM,by = "Gene.Name",all.x = T)
 s_TPM = s_TPM[complete.cases(s_TPM),]
@@ -21,7 +25,7 @@ svg('all_cc_vs_hE_hT_riboTPMbox.svg')
 p2 <- boxplot(s_TPM$ribo_TPM,hE_hT$ribo_TPM,TPM$TPM,log = 'y',
               names=c('all_CC','hE_hT','global_gene'),col=c("red","yellow","blue"),ylab = 'ribo_TPM')
 dev.off()
-hE_hT_only_name = hE_hT[,-c(2,3)]
+hE_hT_only_name = hE_hT[,-c(1,3,4)]
 write.table(hE_hT_only_name,file = "SRR6930636_hE_hT_only_name.txt",sep = '\t',quote = F,row.names = F,col.names = F)
 #=======================================================================================================
 # Extract GO three classification most significant set,then get their intersection.
@@ -51,4 +55,5 @@ p4 <- boxplot(ss_TPM$ribo_TPM,h_e_s_TPM$ribo_TPM,s_e_s_TPM$ribo_TPM,TPM$ribo_TPM
               names=c('inter_inter','hEhT_exc_inter','inter_exc_inter','global_gene'),
               col=c("red","yellow",'blue','green'),ylab = 'riboTPM')
 dev.off()
+
 

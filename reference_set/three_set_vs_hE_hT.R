4db5daf2fdb4e69786bf7c4dd9bf2f2f8445a68b
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-10-11 09:31:01 +0800

    Update three_set_vs_hE_hT.R

--- reference_set/three_set_vs_hE_hT.R
+++ reference_set/three_set_vs_hE_hT.R
@@ -1,26 +1,32 @@
 #================================================================================================
-# 2019-10-07.Modified date:2019-10-09.DYY.Extract GO three classification most significant set,
+# 2019-10-07.Modified date:2019-10-10.DYY.Extract GO three classification most significant set,
 # then get their intersection,except the intersection between MF & BP.
 # The TPM value of the intersection gene is compared with the TPM value of the gene with high 
 # expression & high translation.
 #================================================================================================
+sra = 'SRR6930636'
+species = 'Drosophila_melanogaster'
+experiment = 'experiment3'
+sra_path = paste0('/home/hp/Desktop/other_riboseq/',species,'/',experiment,'/aligned/ribo_num/')
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
 s = read.table("3_completion_set.txt",header = F,quote = '',sep = '\t')
-TPM =  read.table('SRR1804340_riboNum.txt',header = T,quote = '',sep = '\t')
-hE_hT = read.table('SRR1804340_hE_ht_gene.txt',header = T,quote = '',sep = '\t')
+TPM =  read.table(paste0(sra_path,sra,'_riboNum.txt'),header = T,quote = '',sep = '\t')
+hE_hT = read.table(paste0(sra_path,sra,'_hiE_ht_gene.txt'),header = T,quote = '',sep = '\t')
 names(s) = "Gene.Name"
 s_TPM = merge(s,TPM,by = "Gene.Name",all.x = T)
 s_TPM = s_TPM[complete.cases(s_TPM),]
 s_TPM = unique(s_TPM)
 
-svg('40_three_set_vs_hE_hT_TPMbox.svg')
+svg('three_set_vs_hE_hT_TPMbox.svg')
 p1 <- boxplot(s_TPM$TPM,hE_hT$TPM,TPM$TPM,log = 'y',
              names=c('three_intersection','hE_hT','global_gene'),col=c("red","yellow","blue"),ylab = 'TPM')
 dev.off()
-svg('40_three_set_vs_hE_hT_riboTPMbox.svg')
+svg('three_set_vs_hE_hT_riboTPMbox.svg')
 p2 <- boxplot(s_TPM$ribo_TPM,hE_hT$ribo_TPM,TPM$TPM,log = 'y',
              names=c('three_intersection','hE_hT','global_gene'),col=c("red","yellow","blue"),ylab = 'ribo_TPM')
 dev.off()
+hE_hT_only_name = hE_hT[,-c(1,3,4)]
+write.table(hE_hT_only_name,file = "hE_hT_only_name.txt",sep = '\t',quote = F,row.names = F,col.names = F)
 #=======================================================================================================
 # Extract GO three classification most significant set,then get their intersection.
 # The TPM value of the intersection gene is compared with the TPM value of the gene with high 
@@ -49,5 +55,6 @@ p4 <- boxplot(ss_TPM$ribo_TPM,h_e_s_TPM$ribo_TPM,s_e_s_TPM$ribo_TPM,TPM$ribo_TPM
              names=c('inter_inter','hEhT_exc_inter','inter_exc_inter','global_gene'),
              col=c("red","yellow",'blue','green'),ylab = 'riboTPM')
 dev.off()
+
 
 

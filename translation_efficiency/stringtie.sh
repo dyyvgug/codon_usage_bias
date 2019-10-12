ff3c42b9602dfe6b2bf535c5574cab96995a9cf7
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-10-12 09:46:26 +0800

    Update stringtie.sh

--- translation_efficiency/stringtie.sh
+++ translation_efficiency/stringtie.sh
@@ -8,7 +8,7 @@ do
 	samtools sort -@ 8 -o ${item%.*}.bam ${item%.*}.sam
 	
 	echo "str_${item%.*}"
-	stringtie -p 30 --rf -e -G /media/hp/disk1/DYY/reference/annotation/hg19/ref.gtf -o ./${item%.*}.gtf -l ${item%.*} ${item%.*}.bam -A ${item%.*}_abund.out
+	stringtie -p 30 --rf -e -G /media/hp/disk1/DYY/reference/annotation/$1/ref.gtf -o ./${item%.*}.gtf -l ${item%.*} ${item%.*}.bam -A ${item%.*}_abund.out
 
 done
 

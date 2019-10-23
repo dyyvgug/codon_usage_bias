75a3a8795711909835f1fd281aa6a9d085abf4fc
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-10-23 17:11:15 +0800

    Update ribo_compare.R

--- translation_efficiency/ribo_compare.R
+++ translation_efficiency/ribo_compare.R
@@ -2,21 +2,20 @@
 # and the ribosome encoding gene in other genes except this part of the gene were 
 # observed, and the difference of TPM value and TPM value of gene expression between 
 # the two groups of ribosomal genes was observed.
-species = "C_elegans_Ensl_WBcel235"
-setwd(paste0("~/Desktop/other_riboseq/",species,"/experiment2/aligned/ribo_num"))
-other_ribo_gene = read.table("40_other_ribo_filter.txt")
-hE_hT_ribo_gene = read.table("40_hE_ht__ribo_filter.txt")
-other_ribo_gene = cbind(other_ribo_gene,1)
-hE_hT_ribo_gene = cbind(hE_hT_ribo_gene,2)
-names(other_ribo_gene) = c("ribo_name","RNA_TPM","ribo_TPM","tag")
-names(hE_hT_ribo_gene) = c("ribo_name","RNA_TPM","ribo_TPM","tag")
-ribo_gene = merge(other_ribo_gene,hE_hT_ribo_gene,all = T)
-png("40_compare_ribo_rnaTPM.png")
-p <- boxplot(log(other_ribo_gene$RNA_TPM),log(hE_hT_ribo_gene$RNA_TPM),
-             names=c('OTHER','hiEXPhiTRA'),col=c("green","red"),ylab = 'log(TPM)')
+species = "mouse_mm10"
+setwd(paste0("~/Desktop/other_riboseq/",species,"/experiment1/aligned/ribo_num"))
+other_ribo_gene = read.table("other_ribo_filter.txt")
+hE_hT_ribo_gene = read.table("hE_ht__ribo_filter.txt")
+
+names(other_ribo_gene) = c("gene_ID","ribo_name","RNA_TPM","ribo_TPM")
+names(hE_hT_ribo_gene) = c("gene_ID","ribo_name","RNA_TPM","ribo_TPM")
+
+png("compare_ribo_rnaTPM.png")
+pr1 <- boxplot(other_ribo_gene$RNA_TPM,hE_hT_ribo_gene$RNA_TPM,log ="y",
+             names=c('other_ribo','hE_hT_ribo'),col=c("green","red"),ylab = 'RNAseq_TPM')
 dev.off()
-png("40_compare_ribo_riboTPM.png")
-p <- boxplot(log(other_ribo_gene$ribo_TPM),log(hE_hT_ribo_gene$ribo_TPM),
-             names=c('OTHER','hiEXPhiTRA'),col=c("green","red"),ylab = 'log(TPM)')
+png("compare_ribo_riboTPM.png")
+pr2 <- boxplot(other_ribo_gene$ribo_TPM,hE_hT_ribo_gene$ribo_TPM,log ="y",
+             names=c('other_ribo','hE_hT_ribo'),col=c("green","red"),ylab = 'Ribo_seq_TPM')
 dev.off()
 

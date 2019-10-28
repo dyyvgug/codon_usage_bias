44893c06d678a934daf3a983e61bd93c27fc2dd6
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-10-28 16:31:06 +0800

    Update compa_codon_RSCU.R

--- reference_set/compa_codon_RSCU.R
+++ reference_set/compa_codon_RSCU.R
@@ -1,5 +1,5 @@
 #===========================================================================================================
-# 2019-9-19.Modified date:2019-10-24.Author:Dong Yingying.Differences in codon frequency and RSCU value 
+# 2019-9-19.Modified date:2019-10-28.Author:Dong Yingying.Differences in codon frequency and RSCU value 
 #  between DNA sequence codon frequencies of genes that high express high translation and DNA sequences 
 #  of genes with low expression and low translation.
 #===========================================================================================================
@@ -10,6 +10,7 @@ hE_hT_gene_fre = read.table("hE_hT_codon_fre_RSCU.txt",header = T,sep = '\t',quo
 df <- hE_hT_gene_fre
 df$Weights <- ave(df$RSCU,df$AA,FUN=function(x) x/max(x)) # calculate weight
 df = df[,-c(1,3,4,5)]
+df = df[-c(30,61,62,63,64),]
 write.table(df,file = "hE_hT_RSCU_weight.txt",sep = '\t',quote = F,row.names = F,col.names = F) # for calculate CAI
 
 lE_lT_gene_fre = read.table("lE_lT_codon_fre_RSCU.txt",header = T,sep = '\t',quote = "")
@@ -40,12 +41,13 @@ codon_RSCU = merge(hE_hT_RSCU,lE_lT_RSCU,by = "codon")
 codon_RSCU = merge(codon_RSCU,global_RSCU,by = "codon")
 codon_RSCU = merge(codon_RSCU,hEhT_rm_ribo_RSCU,by = "codon")
 codon_RSCU = merge(codon_RSCU,hEhT_RIBO_only_RSCU,by = "codon")
-rownames(codon_RSCU) = codon_RSCU[,1]
-codon_RSCU = codon_RSCU[,-c(1,2)]
+codon_RSCU = codon_RSCU[order(codon_RSCU$AA,decreasing = F),] # order by AA.
+codon_RSCU$codon = gsub(" ", "",codon_RSCU$codon)
+codon_RSCU$AA_codon = paste(codon_RSCU$AA,codon_RSCU$codon,sep = "_") # AA_codon
+rownames(codon_RSCU) = codon_RSCU[,8] # AA_codon as row name
+codon_RSCU = codon_RSCU[,-c(1,2,8)]
 
-pdf("40_codon_RSCU.pdf")
+pdf(file = "40_codon_RSCU.pdf")
 pheatmap(codon_RSCU,cluster_row = FALSE,cluster_cols = FALSE,
          color = colorRampPalette(c("red", "black", "green"))(50))
-
 dev.off() 
-

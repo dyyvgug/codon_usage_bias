4c2ac0ed4ca1e6e774fe07b630a09b10fb487654
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-11-05 10:37:31 +0800

    Update sed_gtf.sh

--- modified_CAI/sed_gtf.sh
+++ modified_CAI/sed_gtf.sh
@@ -5,3 +5,10 @@ sed -i 's/"; /\t/g' ref.gtf
 sed -i 's/transcript_id "//g' ref.gtf
 sed -i 's/gene_name "//g' ref.gtf
 sed -i 's/";//g' ref.gtf
+
+#-------------2-----------------
+sed -i 's/transcript_id \".*\"; gene_id//g' ref.gtf
+sed -i 's/"; gene_name "/\t/g' ref.gtf
+sed -i 's/";//g' ref.gtf
+sed -i 's/"//g' ref.gtf
+sed -i 's/ //g' ref.gtf

aea7cbb63d936fab69eb2a3b34db9bc2dd630fda
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-11-08 14:55:34 +0800

    Update sed_gtf.sh

--- modified_CAI/sed_gtf.sh
+++ modified_CAI/sed_gtf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#--------------1----------------- Ec
 sed -i 's/ geneID ".*";//g' ref.gtf
 sed -i 's/"; gene_id \".*\"; /\t/g' ref.gtf
 sed -i 's/"; /\t/g' ref.gtf
@@ -6,9 +7,24 @@ sed -i 's/transcript_id "//g' ref.gtf
 sed -i 's/gene_name "//g' ref.gtf
 sed -i 's/";//g' ref.gtf
 
-#-------------2-----------------
+#-------------2----------------- Sc Dm includeCe etc.  Most species.
 sed -i 's/transcript_id \".*\"; gene_id//g' ref.gtf
 sed -i 's/"; gene_name "/\t/g' ref.gtf
 sed -i 's/";//g' ref.gtf
 sed -i 's/"//g' ref.gtf
 sed -i 's/ //g' ref.gtf
+
+#-------------3----------------- Ce
+sed -i '/transcript_id \"rna-NC.*/d' ref.gtf
+sed -i 's/transcript_id \".*\"; gene_id//g' ref.gtf
+sed -i 's/"; gene_name "/\t/g' ref.gtf
+sed -i 's/";//g' ref.gtf
+sed -i 's/"//g' ref.gtf
+sed -i 's/ //g' ref.gtf
+
+#-------------4------------------ Mm
+sed -i 's/\"; gene_type \".*\"; gene_name \"/\t/g' ref.gtf
+sed -i '/exon/d' ref.gtf
+sed -i '/transcript/d' ref.gtf
+sed -i 's/\"; level.*//g' ref.gtf
+sed -i 's/gene_id "//g' ref.gtf

3b4be920d253566ef1e8da7026e32f8cc2beb79b
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-11-27 09:53:16 +0800

    Create sed_tRNA_count.sh

---
+++ tRNA-seq/GSM2323454/sed_tRNA_count.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+for i in $(ls *.txt)
+do
+	echo "$i"	
+	sed -i 's/Mus.*-//g' $i
+	sed -i 's/  /\t/g' $i
+	sed -i 's/Val.*\t/Val\t/g' $i
+	sed -i 's/Asp.*\t/Asp\t/g' $i
+	sed -i 's/Met.*\t/Met\t/g' $i
+	sed -i 's/Gly.*\t/Gly\t/g' $i
+	sed -i 's/Gln.*\t/Gln\t/g' $i
+	sed -i 's/Phe.*\t/Phe\t/g' $i
+	sed -i 's/Lys.*\t/Lys\t/g' $i
+	sed -i 's/His.*\t/His\t/g' $i
+	sed -i 's/Glu.*\t/Glu\t/g' $i
+	sed -i 's/Leu.*\t/Leu\t/g' $i
+	sed -i 's/Asn.*\t/Asn\t/g' $i
+	sed -i 's/Arg.*\t/Arg\t/g' $i
+	sed -i 's/Ser.*\t/Ser\t/g' $i
+	sed -i 's/Ala.*\t/Ala\t/g' $i
+	sed -i 's/Ile.*\t/Ile\t/g' $i
+	sed -i 's/Thr.*\t/Thr\t/g' $i
+	sed -i 's/Trp.*\t/Trp\t/g' $i
+	sed -i 's/Pro.*\t/Pro\t/g' $i
+	sed -i 's/Tyr.*\t/Tyr\t/g' $i
+	sed -i 's/Cys.*\t/Cys\t/g' $i
+	sed -i 's/SeC.*\t/STOP\t/g' $i
+	sed -i 's/Sup.*\t/STOP\t/g' $i
+	sed -i '/Undet/d' $i
+	sed -i 's/total: //g' $i
+
+
+done

a5efe36c669719873ef9fadc7cfb22afc4edc28c
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-12-05 21:22:02 +0800

    Add files via upload

---
+++ species_CUB_TPM/CAI_TPM_v3.1_byJCAT.R
@@ -0,0 +1,86 @@
+#========================================================================================================
+#2019-4-12.Modified date:2019-10-17.Author:Yingying Dong.Correlation analysis of CAI by JCat and 
+# global gene expression in all samples.
+#========================================================================================================
+library(getopt)
+library(ggplot2)
+library(MASS)
+library(scales)
+command=matrix(c("species","s",1,"character",
+                 "experiment","e",1,"numeric",
+                 "help","h",0,"logical"),byrow=T,ncol=4)
+args=getopt(command)
+if (!is.null(args$help) || is.null(args$species) || is.null(args$experiment)) {
+  cat(paste(getopt(command, usage = T), "\n"))
+  q()
+}
+species = args$species
+exp = args$experiment
+
+#species = "C_elegans_Ensl_WBcel235"
+#exp = "1"
+
+setwd(paste0("/media/hp/disk1/DYY/reference/annotation/", species))
+cai  = read.table(paste("/media/hp/disk1/DYY/reference/annotation/", 
+                        species, "/ref/CAI_byJCAT.txt", sep = ""), sep = "\t", header = F,quote = '')
+dir.create(paste0("picture_byJCAT",exp))
+dir.create(paste0("correlation_byJCAT",exp))
+setwd(paste0("/media/hp/Katniss/DYY/aligned/",species,"/experiment",exp,"/"))
+gtf_array = list.files(getwd(),pattern = "[SE]RR\\d.+out$") 
+gtf_array
+#graphics.off()
+for (i in gtf_array) {
+  if(FALSE) # examination
+  { 
+    gtf = read.table("SRR6815557_abund.out", sep = "\t", header=T,quote = "",fill = T)
+    name = "SRR6815557"
+  }
+  #write.table(gtf$gene_id,file = "id_wait.txt",quote = F,row.names = F, col.names = F)
+  gtf = read.table(i, sep = "\t", header = T,quote = "")
+  name = sub("^([^.]*).*", "\\1",i) 
+  name = sub("_abund","",name)
+  gtf = gtf[,-c(2,3,4,5,6,7)]
+  
+  names(cai) = c("gene_id","CAI")
+  cai$gene_id = sub(" ","",cai$gene_id)
+  df = merge(cai, gtf, by.x = "gene_id",by.y = "Gene.ID", all = T)
+  df[df == 0] <- NA
+  df = df[complete.cases(df),] # Or df = na.omit(df)
+  #===========================================================================
+  # Correlation_CAI_TPM 
+  #===========================================================================
+  svg(file=paste0("/media/hp/disk1/DYY/reference/annotation/", species,
+                   "/picture_byJCAT",exp,"/",name,"_CAI_TPM.svg"))
+  CAI_cor = cor(df$CAI,df$TPM)
+  CAI_cor
+  CAI_cor_p = cor.test(df$CAI,df$TPM)
+  p <- ggplot(df,aes(x = df$CAI ,y = df$TPM))+
+    geom_point(shape = 16,size = 0.75)+
+    labs(title = paste0(name,"cor_mCAI_TPM    ","r=",round(CAI_cor,3),"  p=",round(CAI_cor_p$p.value,5)))+
+    scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    annotation_logticks(sides="bl")+
+    stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "red",size = 0.75)+
+    theme_bw()+
+    xlab("CAI(JCat) value")+
+    ylab("RNA-seq (TPM)")+
+    theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
+  print(p)
+  dev.off()
+  #===============================================================================
+  # Write out data,convenient to calculate the average
+  #===============================================================================   
+  write.table(CAI_cor,file =  paste0
+              ("/media/hp/disk1/DYY/reference/annotation/", species,
+                "/correlation_byJCAT",exp,"/","CAI_cor.txt"),append = T,quote = FALSE,
+              row.names = F, col.names = F)
+}
+setwd(paste0("/media/hp/disk1/DYY/reference/annotation/",species,"/correlation_byJCAT",exp,"/" ))
+a = read.table("CAI_cor.txt",sep = '\t',header = F)
+
+aveCAI = mean(a$V1)
+aveCAI
+
+write.table(aveCAI,file = "mean_CAI.txt",
+            quote = FALSE,row.names = "mean_correlation", 
+            col.names = "\tCAI_TPM")

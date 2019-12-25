5e8eb290090472ea9ae899fb208d54493f0f8cf3
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-12-25 13:57:10 +0800

    Add files via upload

---
+++ tRNA-seq/GSM2323454_Mm/sed_tRNA_count.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+for i in $(ls *.txt)
+do
+	echo "$i"	
+	sed -i 's/  /\t/g' $i
+	sed -i 's/.*Val//g' $i
+	sed -i 's/.*Asp//g' $i
+	sed -i 's/.*Met//g' $i
+	sed -i 's/.*Gly//g' $i
+	sed -i 's/.*Gln//g' $i
+	sed -i 's/.*Phe//g' $i
+	sed -i 's/.*Lys//g' $i
+	sed -i 's/.*His//g' $i
+	sed -i 's/.*Glu//g' $i
+	sed -i 's/.*Leu//g' $i
+	sed -i 's/.*Asn//g' $i
+	sed -i 's/.*Arg//g' $i
+	sed -i 's/.*Ser//g' $i
+	sed -i 's/.*Ala//g' $i
+	sed -i 's/.*Ile//g' $i
+	sed -i 's/.*Thr//g' $i
+	sed -i 's/.*Trp//g' $i
+	sed -i 's/.*Pro//g' $i
+	sed -i 's/.*Tyr//g' $i
+	sed -i 's/.*Cys//g' $i
+	sed -i 's/.*SeC//g' $i
+	sed -i 's/.*Sup//g' $i
+	sed -i '/Undet/d' $i
+	sed -i 's/total: //g' $i
+
+
+done

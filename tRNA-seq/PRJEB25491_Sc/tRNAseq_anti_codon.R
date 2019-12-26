c85431d5efdf3d1505ed1e7f2753915ad3265c56
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-12-26 14:08:56 +0800

    Update tRNAseq_anti_codon.R

--- tRNA-seq/PRJEB25491_Sc/tRNAseq_anti_codon.R
+++ tRNA-seq/PRJEB25491_Sc/tRNAseq_anti_codon.R
@@ -20,7 +20,7 @@ rscu = read.table("/media/hp/disk1/DYY/reference/annotation/Saccharomyces_cerevi
                   header = T, stringsAsFactors = F)
 
 bed_rscu = merge(b2,rscu,by = "codon", all = T)
-bed_rscu = bed_rscu[-grep("N", bed_rscu$anticodon),]
+bed_rscu = bed_rscu[-grep("NNN", bed_rscu$anticodon),]
 bed_rscu = bed_rscu[-grep("STOP",bed_rscu$AA),]
 bed_rscu = bed_rscu[-grep("M",bed_rscu$AA),]
 bed_rscu = bed_rscu[-grep("W",bed_rscu$AA),]
@@ -39,33 +39,36 @@ p1 <- ggplot(bed_rscu,aes(x = log2(bed_rscu$anti_fre),y = bed_rscu$hits,color = 
   annotation_logticks(sides="bl")+
   stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "black",size = 0.75)+
   theme_bw()+
-  xlab("Log2 (anticodon frequency)")+
-  ylab("Reference set codon frequency")+
+  xlab("Log2 (anticodon count)")+
+  ylab("Reference set codon count")+
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
-        axis.title.x =element_text(size=15), axis.title.y=element_text(size=15))
+        axis.title.x =element_text(size=16), axis.title.y=element_text(size=16))
 p1
-ggsave("Sc_anticodon_codon.pdf",width = 30, height = 25, units = "cm")
+ggsave("Sc_anticodon_codon.pdf")
 write.table(bed_rscu,"tRNAseq_anti_ref_codon.txt",sep = '\t',quote = F,row.names = F)
 
 whole = read.table("/media/hp/disk1/DYY/reference/annotation/Saccharomyces_cerevisiae/ref/codon_frequency.txt")
 names(whole) = c("codon","aa","whole_fre")
 bed_whole = merge(b2,whole,by = "codon")
+bed_whole = bed_whole[-grep("M",bed_whole$aa),]
+bed_whole = bed_whole[-grep("W",bed_whole$aa),]
+
 anti_whole_p = cor.test(log2(bed_whole$anti_fre),bed_whole$whole_fre)
 anti_whole_p
 p2 <- ggplot(bed_whole,aes(x = log2(bed_whole$anti_fre),y = bed_whole$whole_fre,color = bed_whole$aa))+
-  geom_point(shape = 18,size = 2.5)+
+  geom_point(shape = 16,size = 2.5)+
   labs(title = paste0(spA," cor_anticodon_genome_codon    ","r=",round(anti_whole_p$estimate,5),"  p=",round(anti_whole_p$p.value,5)))+
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   annotation_logticks(sides="bl")+
   stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "black",size = 0.75)+
   theme_bw()+
-  xlab("Log2 (anticodon frequency)")+
-  ylab("Whole genome codon frequency")+
+  xlab("Log2 (anticodon count)")+
+  ylab("Whole genome codon count")+
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
-        axis.title.x =element_text(size=15), axis.title.y=element_text(size=15))
+        axis.title.x =element_text(size=16), axis.title.y=element_text(size=16))
 p2
-ggsave("Sc_anticodon_genome_codon.pdf",width = 30, height = 25, units = "cm")
+ggsave("Sc_anticodon_genome_codon.pdf")
 write.table(bed_rscu,"tRNAseq_anti_genome_codon.txt",sep = '\t',quote = F,row.names = F)
 #==================================================================================================
 # Eight major degenerate codon pairs
@@ -90,3 +93,4 @@ names(df4) = c("codon","deg","ribo_hits","anti_fre","AA")
 cor_df4 = cor.test(df4$ribo_hits,log2(df4$anti_fre))
 cor_df4
 plot(df4$ribo_hits,df4$anti_fre,log = "xy")
+

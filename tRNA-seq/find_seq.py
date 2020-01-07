f3c0b2f565a7b319b6ae23efa2481a5c3f2e11c4
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2020-01-07 11:21:22 +0800

    Update find_seq.py

--- tRNA-seq/find_seq.py
+++ tRNA-seq/find_seq.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
 #coding:utf-8
 #=========================================================================================================
-# Yingying Dong.2019-10-30.Get gene sequence of high expression high translation from CDS sequence.
+# Yingying Dong.2019-10-30.Modified date:2020-1-7.Get gene sequence of high expression high translation from CDS sequence.
 #=========================================================================================================
 import os
 import argparse
 
 parser = argparse.ArgumentParser(description='find DNA sequence',prog='find seq',usage='%(prog)s [options]')
 parser.add_argument('--spe', nargs='?',type=str,help='species name')
+parser.add_argument('--samp', nargs='?',type=str,help='sample name')
+parser.add_argument('--exp', nargs='?',type=str,help='experiment lab number')
+parser.add_argument('--per', nargs='?',type=str,help='expression level')
 args = parser.parse_args()
 
 
 os.chdir('/media/hp/disk1/DYY/reference/annotation/{}/ref'.format(args.spe))
 DNA = open('CDS_DNA.fa','r')
-hE_path = '/media/hp/Katniss/DYY/aligned/Saccharomyces_cerevisiae/experiment3/'
-hE = open('{}SRR5422018_low30_exp_only_name.txt'.format(hE_path),'r')
-hE_seq = open('{}SRR5422018_low30_seq.fa'.format(hE_path),'w')
+hE_path = '/media/hp/Katniss/DYY/aligned/{}/experiment{}/'.format(args.spe,args.exp)
+hE = open('{}{}_{}_exp_only_name.txt'.format(hE_path,args.samp,args.per),'r')
+hE_seq = open('{}{}_{}_seq.fa'.format(hE_path,args.samp,args.per),'w')
 
 hE_table = []
 database = {}

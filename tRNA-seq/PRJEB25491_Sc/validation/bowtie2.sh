133c4099a00a993be4568157c1214cd7398443c3
KatnissD <dyyvgug@163.com>
2021-11-18 21:47:17 +0100

    add

--- tRNA-seq/PRJEB25491_Sc/validation/bowtie2.sh
+++ tRNA-seq/PRJEB25491_Sc/validation/bowtie2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-for item in $(ls *.fastq)
+mkdir ./aligned
+for item in $(ls *.sra)
 do
         echo "bowtie2_${item%.*}"
-        bowtie2 --sensitive -p 60 -N 1 -x ../sacCer3-tRNAs/index_one/tRNA_index -U ./${item%.*}.fastq -S ../aligned_tRNA_one/${item%.*}.sam 2>> ../aligned_tRNA_one/mapping_repo.txt
-        
+        bowtie2 --sensitive -p 60 -N 1 -x ./sacCer3-tRNAs/index_one/tRNA_index -U ./clipper_fastq/${item%.*}_trimmed.fq -S ./aligned/${item%.*}.sam 2>> ./aligned/mapping_repo.txt
+
 done
--- tRNA-seq/PRJEB25491_Sc/validation/toBamBed.sh
+++ tRNA-seq/PRJEB25491_Sc/validation/toBamBed.sh
@@ -6,6 +6,8 @@ do
 	echo "sam_${item%.*}"		
 	
 	samtools sort -@ 8 -o ${item%.*}.bam ${item%.*}.sam
-
 	
+	samtools flagstat ${item%.*}.bam > ${item%.*}.bam.flag
+	
+	bamToBed -i ${item%.*}.bam > ${item%.*}.bed
 done

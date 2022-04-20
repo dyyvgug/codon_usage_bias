3ca219109e24037b6776680c2fbca4bbb9ab49ee
KatnissD <dyyvgug@163.com>
2022-04-20 17:51:07 +0200

    add

---
+++ tRNA-seq/GSE66550_Hs/tRNAseq_abti_codon_hg.R
@@ -0,0 +1,162 @@
+#==========================================================================================
+# Yingying Dong.2019-12-18.Modification data: 2022-4.
+#  tRNA seq anticodon & the reference set codon
+#==========================================================================================
+library(dplyr)
+require(Biostrings)
+library(ggplot2)
+spA = "Hs"
+spe = "hg38"
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# GSM1624820 demethylases 82.06%	mapped rate
+tRNA_array = list.files(getwd(),pattern = "txt$") 
+tRNA_array
+for(i in tRNA_array){
+  if(FALSE){
+    tRNA = read.table('GSM1624820.txt',sep = '\t',header = T)
+    name = 'GSM1624820'
+  }
+  tRNA = read.table(i, sep = "\t",header = T)
+  name = sub("^([^.]*).*", "\\1",i) 
+  
+  names(tRNA) = c('anticodon','anti_fre')
+  tRNA$anticodon = sub('(e)TCA',"TCA",tRNA$anticodon,fixed = TRUE)  # Ignore regular expressions.
+  
+  t = tRNA %>% group_by(anticodon) %>% summarise(Frq = sum(anti_fre))
+  t$codon = as.character(reverseComplement(DNAStringSet(t$anticodon)))
+  
+  rscu = read.table(paste0(pathw,"hE_codon_fre_RSCU.txt"),
+                    header = T, stringsAsFactors = F)
+  
+  tRNA_rscu = merge(t,rscu,by = "codon", all = T)
+  tRNA_rscu = tRNA_rscu[-grep("STOP",tRNA_rscu$AA),]
+  tRNA_rscu = tRNA_rscu[-grep("M",tRNA_rscu$AA),]
+  tRNA_rscu = tRNA_rscu[-grep("W",tRNA_rscu$AA),]
+  tRNA_rscu = tRNA_rscu[-grep('-',tRNA_rscu$codon),]
+  tRNA_rscu$anticodon = as.character(reverseComplement( DNAStringSet(tRNA_rscu$codon) ))
+  tRNA_rscu[is.na(tRNA_rscu)] = 0
+  #write.csv(paste0(pathw,tRNA_rscu),"tRNAseq_anti_hE_codon_full.csv",quote = F,row.names = F)
+  tRNA_rscu[tRNA_rscu == 0] <- NA
+  tRNA_rscu = tRNA_rscu[complete.cases(tRNA_rscu),]
+  
+  anti_rscu_p = cor.test(tRNA_rscu$Frq,tRNA_rscu$hits)
+  anti_rscu_p
+  
+  p1 <- ggplot(tRNA_rscu,aes(x = tRNA_rscu$Frq,y = tRNA_rscu$hits,color = tRNA_rscu$AA))+
+    geom_point(shape = 16,size = 2.5)+
+    labs(title = paste0(spA," cor_anticodon_codon    ","r=",round(anti_rscu_p$estimate,5),"  p=",round(anti_rscu_p$p.value,5)))+
+    scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    annotation_logticks(sides="bl")+
+    stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "black",size = 0.75)+
+    theme_bw()+
+    xlab("anticodon count")+
+    ylab("high expression level genes codon count")+
+    theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+          axis.title.x =element_text(size=15), axis.title.y=element_text(size=15))
+  #p1+geom_text(aes(label=codon), size=4,vjust = 0, nudge_y = 0.1,check_overlap = TRUE)
+  p1
+  ggsave(paste0(pathw,name,"_anticodon_codon.pdf"))
+  write.table(paste0(pathw,tRNA_rscu),"tRNAseq_anti_hE_codon.txt",sep = '\t',quote = F,row.names = F)
+  #------------------------remove large discrete value------------------------------------------
+  tRNA_rscu = tRNA_rscu[-grep("ATC",tRNA_rscu$codon),]
+  tRNA_rscu = tRNA_rscu[-grep("TAT",tRNA_rscu$codon),]
+  tRNA_rscu = tRNA_rscu[-grep("AGT",tRNA_rscu$codon),]
+  
+  anti_rscu_p = cor.test(log2(tRNA_rscu$Frq),log2(tRNA_rscu$hits))
+  anti_rscu_p
+  
+  p3 <- ggplot(tRNA_rscu,aes(x = log2(tRNA_rscu$Frq),y = log2(tRNA_rscu$hits),color = tRNA_rscu$AA))+
+    geom_point(shape = 16,size = 2.5)+
+    labs(title = paste0(spA," cor_anticodon_codon    ","r=",round(anti_rscu_p$estimate,5),"  p=",round(anti_rscu_p$p.value,5)))+
+    annotation_logticks(sides="bl")+
+    stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "black",size = 0.75)+
+    theme_bw()+
+    xlab("log2 (anticodon count)")+
+    ylab("log2 (high expression level genes codon count)")+
+    theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+          axis.title.x =element_text(size=15), axis.title.y=element_text(size=15))
+  p3
+  ggsave(p3,filename = paste0(pathw,'hE_codon_re_dis_anticodon.pdf'))
+  write.table(tRNA_rscu,paste0(pathw,"hE_codon_re_dis_anticodon.txt"),sep = '\t',quote = F,row.names = F)
+  
+  
+  whole = read.table(paste0("/media/hp/disk1/DYY/reference/annotation/",spe,"/ref/codon_frequency.txt"))
+  names(whole) = c("codon","aa","whole_fre")
+  
+  tRNA_whole = merge(t,whole,by = "codon")
+  tRNA_whole = tRNA_whole[-grep("*",tRNA_whole$aa,fixed = TRUE),]
+  tRNA_whole = tRNA_whole[-grep("M",tRNA_whole$aa),]
+  tRNA_whole = tRNA_whole[-grep("W",tRNA_whole$aa),]
+  tRNA_whole[tRNA_whole == 0] <- NA
+  tRNA_whole = tRNA_whole[complete.cases(tRNA_whole),]
+  
+  anti_whole_p = cor.test(tRNA_whole$Frq,tRNA_whole$whole_fre)
+  anti_whole_p
+  p2 <- ggplot(tRNA_whole,aes(x = tRNA_whole$Frq,y = tRNA_whole$whole_fre,color = tRNA_whole$aa))+
+    geom_point(shape = 16,size = 2.5)+
+    labs(title = paste0(spA," cor_anticodon_genome_codon    ","r=",round(anti_whole_p$estimate,5),"  p=",round(anti_whole_p$p.value,5)))+
+    scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    annotation_logticks(sides="bl")+
+    stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "black",size = 0.75)+
+    theme_bw()+
+    xlab("anticodon count")+
+    ylab("Whole genome codon count")+
+    theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+          axis.title.x =element_text(size=15), axis.title.y=element_text(size=15))+
+    geom_text(aes(label=codon), size=4,vjust = 0, nudge_y = 0.1,check_overlap = TRUE)
+  p2
+  ggsave(paste0(pathw,spA,"_anticodon_genome_codon.pdf"))
+  write.table(paste0(pathw,tRNA_whole),"tRNAseq_anti_genome_codon.txt",sep = '\t',quote = F,row.names = F)
+  
+  tRNA_whole = tRNA_whole[-grep("ATC",tRNA_whole$codon),]
+  tRNA_whole = tRNA_whole[-grep("TAT",tRNA_whole$codon),]
+  tRNA_whole = tRNA_whole[-grep("AGT",tRNA_whole$codon),]
+  
+  anti_rscu_p = cor.test(log2(tRNA_whole$Frq),log2(tRNA_whole$whole_fre))
+  anti_rscu_p
+  
+  p4 <- ggplot(tRNA_whole,aes(x = log2(tRNA_whole$Frq),y = log2(tRNA_whole$whole_fre),color = tRNA_whole$aa))+
+    geom_point(shape = 16,size = 2.5)+
+    labs(title = paste0(spA," cor_anticodon_codon    ","r=",round(anti_rscu_p$estimate,5),"  p=",round(anti_rscu_p$p.value,5)))+
+    annotation_logticks(sides="bl")+
+    stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "black",size = 0.75)+
+    theme_bw()+
+    xlab("log2 (anticodon count)")+
+    ylab("log2 (whole genome codon count)")+
+    theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+          axis.title.x =element_text(size=15), axis.title.y=element_text(size=15))
+  p4
+  ggsave(p4,filename = paste0(pathw,'genome_codon_re_dis_anticodon.pdf'))
+  write.table(tRNA_rscu,paste0(pathw,"genome_codon_re_dis_anticodon.txt"),sep = '\t',quote = F,row.names = F)
+  
+#==================================================================================================
+# Eight major degenerate codon pairs
+#==================================================================================================
+  tRNA_rscu$deg = tRNA_rscu$anticodon
+  tRNA_rscu$deg = sub("GGC","AGC",tRNA_rscu$deg)
+  tRNA_rscu$deg = sub("GGG","AGG",tRNA_rscu$deg)
+  tRNA_rscu$deg = sub("GGT","AGT",tRNA_rscu$deg)
+  tRNA_rscu$deg = sub("GAC","AAC",tRNA_rscu$deg)
+  tRNA_rscu$deg = sub("GGA","AGA",tRNA_rscu$deg)
+  tRNA_rscu$deg = sub("GCG","ACG",tRNA_rscu$deg)
+  tRNA_rscu$deg = sub("GAG","AAG",tRNA_rscu$deg)
+  tRNA_rscu$deg = sub("GAT","AAT",tRNA_rscu$deg)
+  df <- data.frame(tRNA_rscu$AA,tRNA_rscu$hits,tRNA_rscu$anti_fre,tRNA_rscu$deg)
+  names(df) = c("AA","ribo_hits","anti_fre","deg")
+  df2 <- df %>% group_by(deg) %>% summarise_at(vars(ribo_hits,anti_fre), list(name = sum)) 
+  df2$codon = as.character(reverseComplement(DNAStringSet(df2$deg)))
+  df3 <- data.frame(tRNA_rscu$codon,tRNA_rscu$AA)
+  names(df3) = c("codon","AA")
+  df4 = merge(df2,df3,by = "codon",all.x = TRUE)
+  names(df4) = c("codon","deg","ribo_hits","anti_fre","AA")
+  cor_df4 = cor.test(df4$ribo_hits,log2(df4$anti_fre))
+  cor_df4
+  plot(df4$ribo_hits,df4$anti_fre,log = "xy")
+
+}
---
+++ tRNA-seq/PRJEB25491_Sc/validation/tRNAseq_anticodon_color_RSCU.R
@@ -0,0 +1,136 @@
+#==========================================================================================
+# Yingying Dong.2019-12-18.modify:2022.tRNA seq anticodon & the reference set codon
+#==========================================================================================
+library(dplyr)
+if (!require("Biostrings"))
+  BiocManager::install("Biostrings")
+require(Biostrings)
+library(ggplot2)
+library(scales)
+spA = "Sc"
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#setwd(paste0("G:\\Ñ§Ï°×¨ÓÃ\\tRNA\\Sc\\seqbackup\\aligned_tRNA_one"))
+bed = read.table("ERR2382482.bed", sep = "\t")
+b1 = as.data.frame(table(bed$V1))
+b1$id = gsub(".+tRNA-", "", b1$Var1)
+b1$id = gsub("-\\d-\\d+$", "", b1$id)
+b1$codon = sub(".+-", "", b1$id)
+
+b2 = b1 %>% group_by(codon) %>% summarise(Frq = sum(Freq))
+b2$Codon = as.character(reverseComplement( DNAStringSet(b2$codon) ))
+names(b2) = c("anticodon","anti_fre","codon")
+
+rscu_array<-c("Cytosolic RP","Mitochondria RP","HTT")
+for (i in rscu_array) {
+  # note
+  if(F){
+    i == "Cytosolic RP"
+    # red
+    i == "Mitochondria RP"
+    # color = rgb(200,175,212,maxColorValue = 255)
+    i == "HTT"
+    # rgb(252,215,24)
+  }
+  
+  if(i == "Cytosolic RP"){
+    j = "red"
+  }
+  if(i == "Mitochondria RP"){
+    j = rgb(200,175,212,maxColorValue = 255)
+  }
+  if(i == "HTT"){
+    j = rgb(252,215,24,maxColorValue = 255)
+  }
+  
+  rscu = read.table(paste0(i,"_codon_fre_RSCU.txt"),header = T, stringsAsFactors = F)
+  bed_rscu = merge(b2,rscu,by = "codon", all = T)
+  bed_rscu = bed_rscu[-grep("NNN", bed_rscu$anticodon),]
+  bed_rscu = bed_rscu[-grep("STOP",bed_rscu$AA),]
+  bed_rscu = bed_rscu[-grep("M",bed_rscu$AA),]
+  bed_rscu = bed_rscu[-grep("W",bed_rscu$AA),]
+  
+  #bed_rscu[is.na(bed_rscu)] = 0
+  bed_rscu = bed_rscu[complete.cases(bed_rscu),]
+  bed_rscu$anticodon2 = as.character(reverseComplement( DNAStringSet(bed_rscu$codon) ))
+  #write.csv(bed_rscu,"tRNAseq_anti_ref_codon_full.csv",quote = F,row.names = F)
+  
+  # correlation between anti-codon frequence and codon frequence
+  anti_rscu_p = cor.test(log2(bed_rscu$anti_fre),bed_rscu$hits)
+  anti_rscu_p
+  
+  # correlation between anti-codon frequence and RSCU
+  anti_rscu_p_rs = cor.test(log2(bed_rscu$anti_fre),bed_rscu$RSCU)
+  anti_rscu_p_rs
+  
+  p1 <- ggplot(bed_rscu,aes(x = log2(bed_rscu$anti_fre),y = bed_rscu$RSCU))+
+    geom_point(shape = 16,size = 2.5,color = j)+
+    labs(title = paste0(spA," cor_anticodon_codon    ","r=",round(anti_rscu_p_rs$estimate,5),"  p=",round(anti_rscu_p_rs$p.value,5)))+
+    scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    annotation_logticks(sides="bl")+
+    stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "black",size = 0.75)+
+    theme_bw()+
+    xlab("Log2 (anticodon counts)")+
+    ylab(paste0("RSCU of ",i))+
+    theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+          axis.title.x = element_text(size=16), axis.title.y = element_text(size=16),
+          axis.text = element_text(size=14))
+  p1
+  ggsave(paste0(i,"_anticodon_RSCU.pdf"),width = 15, height = 10, units = "cm")
+  write.csv(bed_rscu,paste0(i,"_anti_rscu.csv"),quote = F,row.names = F)
+}
+
+
+# whole genome genes
+whole = read.table("CDS_all_codon_fre_RSCU.txt",header = T)
+
+bed_whole = merge(b2,whole,by = "codon")
+#bed_whole = bed_whole[-grep("M",bed_whole$aa),]
+#bed_whole = bed_whole[-grep("W",bed_whole$aa),]
+
+anti_whole_p = cor.test(log2(bed_whole$anti_fre),bed_whole$whole_fre)
+anti_whole_p
+
+anti_whole_rs = cor.test(log2(bed_whole$anti_fre),bed_whole$RSCU)
+anti_whole_rs
+
+p2 <- ggplot(bed_whole,aes(x = log2(bed_whole$anti_fre),y = bed_whole$RSCU))+
+  geom_point(shape = 16,size = 2.5,color = rgb(101,200,208,maxColorValue = 255))+
+  labs(title = paste0(spA," cor_anticodon_genome_codon    ","r=",round(anti_whole_rs$estimate,5),"  p=",round(anti_whole_p$p.value,5)))+
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  annotation_logticks(sides="bl")+
+  stat_smooth(method="lm", se=FALSE,linetype="dashed", color = "black",size = 0.75)+
+  theme_bw()+
+  xlab("Log2 (anticodon counts)")+
+  ylab("RSCU of Whole genome")+
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+        axis.title.x =element_text(size=16), axis.title.y=element_text(size=16),
+        axis.text = element_text(size = 14))
+p2
+ggsave("Sc_genome_codon_RSCU.pdf",width = 15,height = 10,units = "cm")
+write.csv(bed_rscu,"tRNAseq_anti_genome_codon_rscu.txt",quote = F,row.names = F)
+
+#==================================================================================================
+# Eight major degenerate codon pairs
+#==================================================================================================
+bed_rscu$deg = bed_rscu$anticodon
+bed_rscu$deg = sub("GGC","AGC",bed_rscu$deg)
+bed_rscu$deg = sub("GGG","AGG",bed_rscu$deg)
+bed_rscu$deg = sub("GGT","AGT",bed_rscu$deg)
+bed_rscu$deg = sub("GAC","AAC",bed_rscu$deg)
+bed_rscu$deg = sub("GGA","AGA",bed_rscu$deg)
+bed_rscu$deg = sub("GCG","ACG",bed_rscu$deg)
+bed_rscu$deg = sub("GAG","AAG",bed_rscu$deg)
+bed_rscu$deg = sub("GAT","AAT",bed_rscu$deg)
+df <- data.frame(bed_rscu$AA,bed_rscu$hits,bed_rscu$anti_fre,bed_rscu$deg)
+names(df) = c("AA","ribo_hits","anti_fre","deg")
+df2 <- df %>% group_by(deg) %>% summarise_at(vars(ribo_hits,anti_fre), list(name = sum)) 
+df2$codon = as.character(reverseComplement(DNAStringSet(df2$deg)))
+df3 <- data.frame(bed_rscu$codon,bed_rscu$AA)
+names(df3) = c("codon","AA")
+df4 = merge(df2,df3,by = "codon",all.x = TRUE)
+names(df4) = c("codon","deg","ribo_hits","anti_fre","AA")
+cor_df4 = cor.test(df4$ribo_hits,log2(df4$anti_fre))
+cor_df4
+plot(df4$ribo_hits,df4$anti_fre,log = "xy")
